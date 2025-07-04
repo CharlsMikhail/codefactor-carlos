a426a02e668644516be011f581e486973fee0c09
Carlos Mijail Mamani Anccasi <89269010+CharlsMikhail@users.noreply.github.com>
2025-07-04 18:24:16 -0500

    initial commit

---
+++ calculator.js
@@ -0,0 +1,43 @@
+function sum(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) {
+    return 'Error: División por cero';
+  }
+  return a / b;
+}
+
+function operate(operation) {
+  const num1 = parseFloat(document.getElementById("num1").value);
+  const num2 = parseFloat(document.getElementById("num2").value);
+  let result;
+
+  switch (operation) {
+    case 'sum':
+      result = sum(num1, num2);
+      break;
+    case 'subtract':
+      result = subtract(num1, num2);
+      break;
+    case 'multiply':
+      result = multiply(num1, num2);
+      break;
+    case 'divide':
+      result = divide(num1, num2);
+      break;
+    default:
+      result = 'Operación no válida';
+  }
+
+  document.getElementById("result").innerText = `Resultado: ${result}`;
+}
